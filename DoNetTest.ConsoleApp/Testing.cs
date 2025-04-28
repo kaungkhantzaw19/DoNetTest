8bdfaeeee53ce8ab7f6c47674f95c6e391ca9c57
kaungkhantzaw19 <kaungkhantzawsonny@gmail.com>
2025-04-28 20:17:57 +0630

    feat

---
+++ DoNetTest.ConsoleApp/Testing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoNetTest.ConsoleApp
+{
+    internal class Testing
+    {
+        public static void Test()
+        {
+
+        }
+    }
+}
